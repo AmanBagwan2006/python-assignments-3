1db71c00959c7e9711fb3dc165c4f54f0c5ba217
Amanulla Bagwan <bagwanaman2006@gmail.com>
2025-08-24 22:15:35 +0530

    Add files via upload

---
+++ Task_2.py
@@ -0,0 +1,14 @@
+
+
+import math
+
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sin_val = math.sin(num)
+
+print(f"Square root: {sqrt_val}")
+print(f"Logarithm: {log_val}")
+print(f"Sine: {sin_val}")
+
